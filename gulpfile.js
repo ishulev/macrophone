7544202421259ea02a5b897776251ccfe5120d8e
Ivan Shulev <ishulev@melontech.com>
2018-03-02 19:11:46 +0200

    add npm and appropriate modules

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const connect = require('gulp-connect');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        livereload: true
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['./index.html'], function() {
+        return connect.reload();
+    });
+});
+
+gulp.task('default', ['connect', 'watch', 'sass', 'sass:watch']);
